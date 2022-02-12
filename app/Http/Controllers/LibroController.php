c5f242ff3a56820895b3b0d0381ecdfb9bc6ee11
mavuriel <urieljtenorio@gmail.com>
2022-02-11 20:04:56 -0600

    Move function to LibroController

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -1,9 +1,14 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Libro;
 
-class LibroController extends Controller{
+class LibroController extends Controller
+{
+    public function index()
+    {
+        $datosLibro = Libro::all();
 
+        return response()->json($datosLibro);
+    }
 }
